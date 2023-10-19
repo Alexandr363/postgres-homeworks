6bdfefcd24969bca40a624d0fc5b435d2b8fdb73
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-10-19 21:17:07 +0300

    work 1 ready

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+
+CREATE TABLE customers   -- клиент
+(
+    customer_id varchar(30) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE employees   -- сотрудник
+(
+    employee_id int2 PRIMARY KEY,
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30) NOT NULL,
+	title varchar(30) NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE orders    -- заказ
+(
+    order_id int2 PRIMARY KEY,
+	cusmomer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id int2 REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_sity varchar(20) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,44 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(host='localhost',
+                        database='north',
+                        user='postgres',
+                        password='admin')
+
+with conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r',
+                  encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)
+            for row in reader:
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                            (row[0], row[1], row[2]))
+                cur.execute('SELECT * FROM customers')
+
+with conn:
+    with conn.cursor() as cur:
+        with open('north_data/employees_data.csv', 'r',
+                  encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)
+            for row in reader:
+                cur.execute('INSERT INTO employees VALUES '
+                            '(%s, %s, %s, %s, %s, %s)',
+                            (row[0], row[1], row[2], row[3], row[4], row[5]))
+                cur.execute('SELECT * FROM employees')
+
+with conn:
+    with conn.cursor() as cur:
+        with open('north_data/orders_data.csv', 'r',
+                  encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)
+            for row in reader:
+                cur.execute('INSERT INTO orders VALUES '
+                            '(%s, %s, %s, %s, %s)',
+                            (row[0], row[1], row[2], row[3], row[4]))
+                cur.execute('SELECT * FROM orders')
